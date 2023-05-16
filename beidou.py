d93bc578c353d11acec05b2d1e2088bd74c0d09a
Renan Rafael Bertoldo <renan10r_@hotmail.com>
2023-05-16 23:32:20 +0100

    new satellities

---
+++ beidou.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def get_beidou_tles():
+    url = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=beidou&FORMAT=tle'
+    response = requests.get(url)
+    tle_data = response.text
+
+    # Divide os TLEs em linhas
+    tle_lines = tle_data.strip().split('\n')
+
+    # Extrai as informações relevantes de cada TLE
+    tles = []
+    for i in range(0, len(tle_lines), 3):
+        line1 = tle_lines[i].strip()
+        line2 = tle_lines[i + 1].strip()
+
+        if i + 2 < len(tle_lines):
+            line3 = tle_lines[i + 2].strip()
+        else:
+            line3 = ''
+
+        tle = [
+            line1,
+            line2,
+            line3
+        ]
+
+        tles.append(tle)
+
+    return tles
+
+def save_tles_to_json(tles, filename):
+    with open(filename, 'w') as file:
+        json.dump(tles, file, indent=4)
+
+tles = get_beidou_tles()
+save_tles_to_json(tles, 'new_beidou.json')
